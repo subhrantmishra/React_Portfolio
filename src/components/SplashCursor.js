92da3c4d970a341852b2269bda138d6510b4f426
Subhrant Mishra <subhrantmis278@gmail.com>
2026-03-20 22:47:05 +0530

    Update SplashCursor.js

--- src/components/SplashCursor.js
+++ src/components/SplashCursor.js
@@ -2,8 +2,8 @@
 import { useEffect, useRef } from 'react';
 
 function SplashCursor({
-  SIM_RESOLUTION = 128,
-  DYE_RESOLUTION = 1440,
+  SIM_RESOLUTION = 64,
+  DYE_RESOLUTION = 512,
   CAPTURE_RESOLUTION = 512,
   DENSITY_DISSIPATION = 3.5,
   VELOCITY_DISSIPATION = 2,
