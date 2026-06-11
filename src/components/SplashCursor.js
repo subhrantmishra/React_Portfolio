fad03dfa4deb289484b7e9f387e9e0df9f52b304
Subhrant Mishra <subhrantmis278@gmail.com>
2026-06-11 22:15:02 +0530

    Update SplashCursor.js

--- src/components/SplashCursor.js
+++ src/components/SplashCursor.js
@@ -882,9 +882,9 @@ function SplashCursor({
 
     function generateColor() {
       let c = HSVtoRGB(Math.random(), 1.0, 1.0);
-      c.r *= 0.15;
-      c.g *= 0.15;
-      c.b *= 0.15;
+      c.r *= 0.07;
+      c.g *= 0.07;
+      c.b *= 0.07;
       return c;
     }
 
